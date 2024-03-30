663368c65326dcbd7f23a17953ab0e9a7bfae04a
Dipesh Baradiya <dipeshbr25@gmail.com>
2024-03-30 22:17:41 +0530

    header done

---
+++ assets/js/common.js
@@ -0,0 +1,15 @@
+const $body = $('body');
+
+    let oldScroll = 0;
+document.addEventListener('scroll', e => {
+    var wTop = document.documentElement.scrollTop || document.body.scrollTop;
+  
+    //up
+    if (wTop <= 0 || wTop < oldScroll) {
+        $body.removeClass('hideHeader');
+    } else {
+        $body.addClass('hideHeader');
+    }
+  
+    oldScroll = wTop;
+  }, { passive: true });
