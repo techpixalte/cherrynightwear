a75db8bbdba7a147b420a76bc78ec18f19924bd4
Dipesh Baradiya <dipeshbr25@gmail.com>
2024-03-31 22:59:23 +0530

    home page desktop

--- assets/js/common.js
+++ assets/js/common.js
@@ -14,6 +14,8 @@ document.addEventListener('scroll', e => {
     oldScroll = wTop;
   }, { passive: true });
 
+
+//   banner-swiper
   var swiper = new Swiper(".bannerSwiper", {
     autoplay: {
         delay: 5000,
@@ -24,3 +26,76 @@ document.addEventListener('scroll', e => {
             dynamicBullets: true,
         },
     });
+
+    // product-swiper
+
+    var swiper = new Swiper(".productSwiper_1", {
+        slidesPerView: 5,
+        navigation: {
+          nextEl: ".swiper-next-1",
+          prevEl: ".swiper-prev-1",
+        },
+      });
+    var swiper = new Swiper(".productSwiper_2", {
+        slidesPerView: 5,
+        navigation: {
+          nextEl: ".swiper-next-2",
+          prevEl: ".swiper-prev-2",
+        },
+      });
+    var swiper = new Swiper(".productSwiper_3", {
+        slidesPerView: 5,
+        navigation: {
+          nextEl: ".swiper-next-3",
+          prevEl: ".swiper-prev-3",
+        },
+      });
+    var swiper = new Swiper(".productSwiper_4", {
+        slidesPerView: 5,
+        navigation: {
+          nextEl: ".swiper-next-4",
+          prevEl: ".swiper-prev-4",
+        },
+      });
+    var swiper = new Swiper(".productSwiper_5", {
+        slidesPerView: 5,
+        navigation: {
+          nextEl: ".swiper-next-5",
+          prevEl: ".swiper-prev-5",
+        },
+      });
+    var swiper = new Swiper(".productSwiper_6", {
+        slidesPerView: 5,
+        navigation: {
+          nextEl: ".swiper-next-6",
+          prevEl: ".swiper-prev-6",
+        },
+      });
+    var swiper = new Swiper(".productSwiper_7", {
+        slidesPerView: 5,
+        navigation: {
+          nextEl: ".swiper-next-7",
+          prevEl: ".swiper-prev-7",
+        },
+      });
+    var swiper = new Swiper(".productSwiper_8", {
+        slidesPerView: 5,
+        navigation: {
+          nextEl: ".swiper-next-8",
+          prevEl: ".swiper-prev-8",
+        },
+      });
+    var swiper = new Swiper(".productSwiper_9", {
+        slidesPerView: 5,
+        navigation: {
+          nextEl: ".swiper-next-9",
+          prevEl: ".swiper-prev-9",
+        },
+      });
+
+
+      var numericInputs = document.querySelectorAll("[inputmode='numeric']");
+      
+      numericInputs.forEach((input) => {
+        validateInput(input);
+      });
